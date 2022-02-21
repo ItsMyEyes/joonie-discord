91471bd55adbed3b9ce0c56bcc4b8fab6ff79cb5
kiyora <and.syahruddin@gmail.com>
2022-02-21 13:26:42 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Client, Collection, Intents } = require("discord.js");
 const { readdirSync } = require("fs");
+const keepAlive = require("./keepAlive");
 const client = new Client({
    shards: "auto",
-   intents: [Intents.FLAGS.GUILDS, Intents.
-FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
+   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
     allowedMentions: {
         parse: ["everyone", "roles", "users"],
         repliedUser: true
@@ -23,6 +23,7 @@ client.commands = new Collection();
 client.categories = readdirSync("./commands/");
 client.logger = require("./utils/logger.js");
 client.emoji = require("./utils/emoji.json");
+keepAlive() 
 
 require("./handler/Client")(client);
 
